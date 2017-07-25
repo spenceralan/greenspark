dd254e1928e8730feedc7f2e48bebd1429bfd15a
Spencer Alan <spencer.alan@icloud.com>
2017-07-25 12:03:32 -0700

    add full crud

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,3 +1,54 @@
 class StocksController < ApplicationController
+  def index
+    @stocks = Stock.order(:name).page params[:page]
+  end
+
+  def show
+    @stock = Stock.find(params[:id])
+  end
+
+  def new
+    @stock = Stock.new
+    render :new
+  end
+
+  def create
+    @stock = current_user.stocks.new(stock_params)
+    if @stock.save
+      flash[:notice] = "Your stock has been saved."
+      redirect_to stock_path(@stock)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @stock = Stock.find(params[:id])
+  end
+
+  def update
+    @stock = Stock.find(params[:id])
+    if @stock.update(stock_params)
+      flash[:notice] = "The stock has been updated."
+      redirect_to stock_path(@stock)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Stock.find(params[:id]).destroy
+    flash[:notice] = "The stock has been deleted."
+    redirect_to root_path
+  end
+
+
+
+
+
+private
+  def stock_params
+    params.require(:stock).permit(:name, :symbol, :description)
+  end
 
 end
