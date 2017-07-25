8b2132122360467d733adfaec93a0b979a734684
Spencer Alan <spencer.alan@icloud.com>
2017-07-25 13:51:26 -0700

    add default stocks for new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   has_many :stocks, dependent: :destroy
 
+  after_create :default_stocks
+
   devise :database_authenticatable,
     :registerable,
     :recoverable,
@@ -36,5 +38,66 @@ class User < ApplicationRecord
       conditions[:email].downcase! if conditions[:email]
       where(conditions.to_hash).first
     end
+  end
+private
+
+  def default_stocks
+    self.stocks.create([
+      {
+        name: "PowerShares WilderHill Clean Energy Portfolio",
+        symbol: "PBW",
+        description: "The investment seeks investment results that generally correspond (before fees and expenses) to the price and yield of the WilderHill Clean Energy Index. The fund generally will invest at least 90% of its total assets in common stocks of companies that comprise the underlying index. The underlying index was composed of the stocks of about 40 companies that are publicly traded in the United States and that are engaged in the business of the advancement of cleaner energy and conservation.",
+        exchange: "NAR",
+      },
+      {
+        name: "VanEck Vectors Solar Energy ETF",
+        symbol: "KWT",
+        description: "The investment seeks to replicate as closely as possible, before fees and expenses, the price and yield performance of the MVISä Global Solar Energy Index. The fund normally invests at least 80% of its total assets in securities that comprise the fund's benchmark index. The index includes equity securities of companies that generate at least 50% of their revenues from photovoltaic and solar power, solar power equipment/technologies and materials or services to solar power equipment/technologies producers. It is non-diversified.",
+        exchange: "NAR",
+      },
+      {
+        name: "First Trust Global Wind Energy ETF",
+        symbol: "FAN",
+        description: "The investment seeks investment results that correspond generally to the price and yield (before the fund's fees and expenses) of an equity index called the ISE Clean Edge Global Wind Energy Index. The fund will normally invest at least 90% of its net assets (including investment borrowings) in common stocks or in depositary receipts representing securities in the index. The index provides a benchmark for investors interested in tracking public companies throughout the world that are active in the wind energy industry based on analysis of the products and services offered by those companies. The fund is non-diversified.",
+        exchange: "NAR",
+      },
+      {
+        name: "iShares Global Clean Energy ETF",
+        symbol: "ICLN",
+        description: "The investment seeks to track the S&P Global Clean Energy IndexTM. The fund generally invests at least 90% of its assets in the component securities of the index and in investments that have economic characteristics that are substantially identical to the component securities and may invest up to 10% of its assets in certain futures, options and swap contracts, cash and cash equivalents, as well as in securities not included in the index. The index is designed to track the performance of approximately 30 of what are will be the most liquid and tradable securities of global companies involved in clean energy related businesses. The fund is non-diversified.",
+        exchange: "NASDAQ",
+      },
+      {
+        name: "Guggenheim Solar ETF",
+        symbol: "TAN",
+        description: "The investment seeks investment results that correspond generally to the performance, before the fund's fees and expenses, of an equity index called the MAC Global Solar Energy Index. The fund will invest at least 90% of its total assets in common stock, ADRs and GDRs that comprise the index and depositary receipts representing common stocks included in the index. The index is comprised of equity securities, including ADRs and GDRs, traded in developed markets. The fund is non-diversified.",
+        exchange: "NAR",
+      },
+      {
+        name: "PowerShares WilderHill Progressive Energy Portfolio",
+        symbol: "PUW",
+        description: "The investment seeks investment results that generally correspond (before fees and expenses) to the price and yield of the WilderHill Progressive Energy Index. The fund generally will invest at least 90% of its total assets in common stocks that comprise the underlying index. The underlying index was composed of common stocks of about 40 companies that are publicly traded in the United States and are engaged in a business or businesses that the index provider believes may substantially benefit from a societal shift toward the transitional energy technologies significant in improving the use of fossil fuels and nuclear power.",
+        exchange: "NAR",
+      },
+      {
+        name: "First Trust NASDAQ Clean Edge Green Energy Index Fund",
+        symbol: "QCLN",
+        description: "The investment seeks investment results that correspond generally to the price and yield (before the fund's fees and expenses) of an equity index called the NASDAQ® Clean Edge® Green Energy IndexSM. The fund will normally invest at least 90% of its net assets (including investment borrowings) in common stocks that comprise the index. The index is a modified market capitalization-weighted index in which larger companies receive a larger index weighting and includes caps to prevent high concentrations among larger alternative energy stocks. It is non-diversified.",
+        exchange: "NASDAQ",
+      },
+      {
+        name: "PowerShares Cleantech Portfolio ETF",
+        symbol: "PZD",
+        description: "The investment seeks investment results that generally correspond (before fees and expenses) to the price and yield of the Cleantech IndexTM. The fund generally will invest at least 90% of its total assets in stocks of clean technology (or 'cleantech') companies that comprise the underlying index and American depositary receipts ('ADRs') based on the stocks in the underlying index. The underlying index is a modified equally weighted index currently comprised of stocks of publicly traded cleantech companies and ADRs on such stocks.",
+        exchange: "NAR",
+      },
+      {
+        name: "VanEck Vectors Global Alternative Energy ETF",
+        symbol: "GEX",
+        description: "The investment seeks to replicate as closely as possible, before fees and expenses, the price and yield performance of the Ardour Global IndexSM (Extra Liquid). The fund normally invests at least 80% of its total assets in stocks of companies primarily engaged in the business of alternative energy. Such companies may include small- and medium-capitalization companies and foreign issuers. Alternative energy refers to the generation of power through environmentally friendly, non traditional sources. It is non-diversified.",
+        exchange: "NAR",
+      },
+
+    ])
   end
 end
