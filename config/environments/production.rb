c3ec7211ca341a827fc28c7873331f320dff650a
Spencer Alan <spencer.alan@icloud.com>
2017-07-24 22:49:36 -0700

    update host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: 'greenspark.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'www.greenspark.today' }
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
     api_key: ENV['MG_ACTIVE_API'],
