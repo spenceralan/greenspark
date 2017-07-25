bb602c46aa3ef51dc6543393a6d11ce1d2c53b90
Spencer Alan <spencer.alan@icloud.com>
2017-07-25 10:10:46 -0700

    add sign in and out paths

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(users)
+    stocks_path
+  end
+
+  def after_sign_out_path_for(users)
+    root_path
+  end
+
   protected
 
   def configure_permitted_parameters
