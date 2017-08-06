ee16fc6010d2bb2c183526181cf915ff35d26940
Spencer Alan <spencer.alan@icloud.com>
2017-08-05 18:31:39 -0700

    add validations, fix update bug, add transaction type constant

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,15 @@
 class Transaction < ApplicationRecord
+
+  TRANSACTION_TYPES = ["buy", "sell"]
+
   belongs_to :stock
 
-  before_create :update_total
+  before_save :update_total
+
+  validates_presence_of :price, :quantity, :date, :trade_type
+  validates_numericality_of :price, only_integer: false, allow_blank: true
+  validates_numericality_of :quantity, only_integer: true, greater_than: 0, allow_blank: true
+  validates_inclusion_of :trade_type, in: TRANSACTION_TYPES
 
   def update_total
     self.total = self.price * self.quantity
