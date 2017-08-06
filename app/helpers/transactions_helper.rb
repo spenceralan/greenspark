aca8c31371b2d114f8d30cc27d0178e02068045d
Spencer Alan <spencer.alan@icloud.com>
2017-08-05 18:31:01 -0700

    add formatting methods for price and select

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -4,4 +4,14 @@ module TransactionsHelper
     Date.today.strftime("%Y-%m-%d")
   end
 
+  def transaction_select_options(transaction_types)
+    transaction_types.map do | type |
+      [type.capitalize, type]
+    end
+  end
+
+  def format_number(number)
+    format("%0.2f", number)
+  end
+
 end
