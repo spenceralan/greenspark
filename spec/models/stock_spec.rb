4d3f907d4c36761beecf505a605535dd0c4927a8
Spencer Alan <spencer.alan@icloud.com>
2017-08-09 14:36:36 -0700

    add stock model specs

--- spec/models/stock_spec.rb
+++ spec/models/stock_spec.rb
@@ -1,2 +1,11 @@
 require 'rails_helper'
 
+describe Stock do
+  it { should belong_to :user }
+  it { should have_many :transactions }
+
+  it { should validate_presence_of :symbol }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :description }
+
+end
