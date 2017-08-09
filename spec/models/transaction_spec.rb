fb4b760a75c8e57776146bbe2bb3a441e1825309
Spencer Alan <spencer.alan@icloud.com>
2017-08-09 13:26:56 -0700

    add transaction model specs

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Transaction do
+  it { should belong_to :stock }
+
+  it { should validate_presence_of :price }
+  it { should validate_presence_of :quantity }
+  it { should validate_presence_of :date }
+  it { should validate_presence_of :trade_type }
+
+  it { should validate_numericality_of(:price) }
+  it { should validate_numericality_of(:quantity).only_integer }
+  it { should validate_numericality_of(:quantity).is_greater_than(0) }
+
+  it { should validate_inclusion_of(:trade_type).in_array(Transaction::TRANSACTION_TYPES) }
 end
