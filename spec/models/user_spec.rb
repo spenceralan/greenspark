10d4e7e3582515d908591db3e96bed781568a3f2
Spencer Alan <spencer.alan@icloud.com>
2017-08-09 11:55:43 -0700

    add user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  it { should validate_presence_of :email }
+  it { should validate_presence_of :username }
+  it { should have_many :stocks }
+
+  it { should validate_uniqueness_of(:email).case_insensitive }
+  it { should validate_uniqueness_of(:username).case_insensitive }
+
 end
+
