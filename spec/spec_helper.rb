c3bc50b894367039f5ce4e7808a0563ffcf02ca5
Spencer Alan <spencer.alan@icloud.com>
2017-08-09 11:34:20 -0700

    simple cov configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,22 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require 'simplecov'
+
+SimpleCov.start
+
 RSpec.configure do |config|
+
+  # simplecov configuration
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
@@ -94,3 +109,5 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 end
+
+
