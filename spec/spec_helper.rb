b8d45fabc0b334757f4fddedddf776b651a36017
Spencer Alan <spencer.alan@icloud.com>
2017-08-09 17:35:27 -0700

    require alpha vantage config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,8 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
 require 'support/simple_cov'
+require 'webmock/rspec'
+require 'support/alpha_vantage'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
