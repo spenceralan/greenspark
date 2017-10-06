f3fa96862e5d0dd73873dfc380ba117e425ef66f
Spencer Alan <spencer.alan@icloud.com>
2017-10-06 10:49:50 -0700

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,60 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+def create_test_user(budget, start_date, frequency)
+
+  User.find_by(username: "testuser#{budget}#{frequency}")&.destroy
+
+  if budget > 100
+    incremental = budget
+  else
+    incremental = 100
+  end
+
+  user = User.create(
+    username: "testuser#{budget}#{frequency}",
+    email: "testuser#{budget}#{frequency}@test.com",
+    password: "test123",
+    password_confirmation: "test123",
+    incremental: incremental
+  )
+  
+  date = start_date
+  
+  while date < Date.today
+    user.stocks.each do |stock|
+      closing_price = stock.ticker.closing_price(date)
+      if closing_price.present?
+        stock.ticker.update_attribute(:last_updated_price, closing_price.first.price)
+  
+      end
+    end
+  
+    stocks_to_buy = Algorithm.new(user, budget)
+  
+    stocks_to_buy.stocks_within_budget.each do |stock|
+      quantity = stock.fetch(:quantity)
+      stock = stock.fetch(:stock)
+      stock.transactions.create!(
+        date: date,
+        quantity: quantity,
+        price: stock.ticker.last_updated_price,
+        trade_type: "buy",
+        user: user
+      )
+    end
+  
+    date += frequency
+  end
+
+end
+
+date = Date.new(2012,1,4)
+
+create_test_user(50, date, 14)
+create_test_user(100, date, 7)
+create_test_user(500, date, 28)
+create_test_user(1000, date, 28)
