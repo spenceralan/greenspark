1182da88830cd48fb8e6f21e6e07522ea6683ffb
Spencer Alan <hello@spenceralan.com>
2018-01-17 21:56:16 -0500

    clean up

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,16 +1,8 @@
 FactoryGirl.define do
-  factory :closing_price do
-    
-  end
-  factory :ticker do
-    
-  end
-
   factory :user do
     username "testuser"
     email  "test@test.com"
     password "123456"
     password_confirmation "123456"
   end
-
 end
